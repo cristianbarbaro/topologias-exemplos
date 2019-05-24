2cb0d9701dd69097d05f77f6ebf7d5f6ae7dbf29
core <you@example.com>
2019-05-24 10:55:12 -0300

    configuraciones rutas guardadas

---
+++ files/n4.bash
@@ -0,0 +1,4 @@
+ifconfig eth0 10.0.3.2/24
+ifconfig eth1 10.0.4.1/24
+ifconfig eth2 10.0.5.1/24
+route add default gw 10.0.4.2
---
+++ files/n7.bash
@@ -0,0 +1,3 @@
+ifconfig eth0 10.0.2.1/24
+ifconfig eth1 10.0.3.1/24
+route add default gw 10.0.3.2
